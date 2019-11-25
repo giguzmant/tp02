1349e195d4eb981bdd02461bc018340f41b34f7c
giguzmant <giselleguzmant@gmail.com>
2019-11-24 21:36:02 -0300

    test ejercicio 4 terminado

--- index.test.js
+++ index.test.js
@@ -116,7 +116,15 @@ const precioMaquina = (monitor,motherboard,hdd,ram) =>{
     Para agregar este dato, tenemos que usar la función desarrollada en el punto 
     anterior obtenerIdVenta */
 
-
+ /*1*/
     test('Devuelve el importe total para armar una maquina',()=>{
         expect(precioMaquina('Monitor GPRS 3000', 'Motherboard ASUS 1500', 'HDD Toyiva', 'RAM Quinston')).toMatch("Para armar una maquina necesitas: $520");
+    });
+ /*2*/
+    test('Devuelve la cantidad de ventas por componente',()=>{
+        expect(cantidadVentasComponente('Motherboard ASUS 1500')).toMatch("Motherboard ASUS 1500 fue vendido 2 veces");
+    });
+ /*4*/
+    test('Devuelve el componente mas vendido',()=>{
+        expect(componenteMasVendido()).toMatch("El componente mas vendido fue: Monitor GPRS 3000");
     });
